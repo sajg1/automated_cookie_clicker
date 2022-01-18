54a5aa2fd9df0a68ec26124ba44ea49d4c57d195
Steve Gossip <stephengossip@gmail.com>
2022-01-18 19:56:13 +0000

    able to automate the cookie clicking for 5 seconds and access the tally of cookies collected.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 # chromedriver must be up to date with latest chrome browser version
 chrome_driver_path = '/Users/admin/Development/chromedriver'
@@ -7,7 +8,22 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element_by_xpath("//*[@id='cookie']")
-cookie.click()
+cookie_tally = driver.find_element_by_xpath('//*[@id="money"]')
+
+
+start_time = time.time()
+check_time = start_time + 5
+running = True
+
+while running:
+    if time.time() > check_time:
+        running = False
+
+    else:
+        cookie.click()
+
+print(cookie_tally.text)
+
 
 
 
